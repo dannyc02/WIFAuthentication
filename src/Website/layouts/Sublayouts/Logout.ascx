ca89442a30e53393bedf3e80d0c3515756b11fda
dannyc02 <danc@aqueduct.co.uk>
2012-08-24 11:09:38 +0100

    ADding logout

---
+++ src/Website/layouts/Sublayouts/Logout.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Logout.ascx.cs" Inherits="Website.layouts.Sublayouts.Logout" %>
+<asp:Button runat="server" ID="LogoutButton" Text="Logout"/>
---
+++ src/Website/layouts/Sublayouts/Logout.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.IdentityModel.Web;
+
+namespace Website.layouts.Sublayouts
+{
+    public partial class Logout : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LogoutButton.Click += new EventHandler(LogoutButton_Click);
+        }
+
+        void LogoutButton_Click(object sender, EventArgs e)
+        {
+            WSFederationAuthenticationModule.FederatedSignOut(null, new Uri("http://poc.local"));
+        }
+    }
+}
